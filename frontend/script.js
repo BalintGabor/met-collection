c4443e7d3ac05584b732b664ad41265f0ffbb963
Gabor Balint <trondheim.bg@gmail.com>
2024-02-12 17:15:50 +0100

    department buttons' eventlistener and deparment images

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,6 @@ const loadEvent = async () => {
     const responseDepartments = await fetch("https://collectionapi.metmuseum.org/public/collection/v1/departments");
     const responseDepartmentsJson = await responseDepartments.json();
     const departments = responseDepartmentsJson.departments;
-    console.log(departments);
 
     // Create departments' container and fill up with the fetched departments
     const departmentContainer = document.querySelector("#departments-container");
@@ -14,12 +13,24 @@ const loadEvent = async () => {
             `<div class="department-card">
                 <button class="department-button" value="${department.departmentId}">
                     <div class="department-image-container">
-                        <img src="./img/departments/dept_${department.departmentId}.jpg" alt="department">
+                        <img src="./images/departments/dept_${department.departmentId}.jpg" alt="department">
                     </div>
                     <h2>${department.displayName}</h2>
                 </button>
             </div>`
         )
+    }
+
+    // Eventlistener for every department button
+    const departmentButtons = document.querySelectorAll(".department-button");
+    for (departmentSearch of departmentButtons) {
+        ((departmentSearch) => {
+            departmentSearch.addEventListener("click", clickFetch);
+            function clickFetch() {
+                let input = departmentSearch.value;
+                console.log(input)
+            }
+        })(departmentSearch);
     }
 }
 
